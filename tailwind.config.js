4593cac572e57f70338df7664f46bc8caadfdeb9
ASahithi2005 <adlasahithi8@gmail.com>
2025-05-16 20:53:50 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this if your source files are elsewhere
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
